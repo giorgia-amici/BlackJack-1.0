dc209d748992f8eeb9ec942eab6b1113110f45c2
Giorgia <giorgia1london@gmail.com>
2014-12-09 17:30:51 +0000

    about to test this rad shit

--- spec/deckSpec.js
+++ spec/deckSpec.js
@@ -7,26 +7,13 @@ describe("Deck", function(){
  		expect(deck.readyDeck).toBeArray;
  	});
 
- 	it('is an array of 4 hashes', function(){
- 		expect(deck.readyDeck.length).toEqual(4)
- 		expect(deck.readyDeck[0]['flowers7']).toEqual(7)
- 		expect(deck.readyDeck[1]['hearts8']).toEqual(8)
- 		expect(deck.readyDeck[2]['spades9']).toEqual(9)
- 		expect(deck.readyDeck[3]['diamondsA']).toEqual(11)
+ 	it('matches suits with face values', function(){
+ 		deck.makeSuits()
+ 		expect(deck.readyDeck[0]).toEqual('flowers7')
  	});
 
  	it('can pull one at random', function(){
- 		deck.shuffle();
-
+ 		
  	});
 
- 	it('matches suits with face values', function(){
- 		expect(deck.makeSuits()).toEqual()//result
- 	})
 });
-
-//[7, 8, 9, 10, 10, 10, 10, 11] <= values
-
-//[7, 8, 9, 10, J, Q, K, A]
-
-//[flowers, hearts, diamonds, spades] <= suits
