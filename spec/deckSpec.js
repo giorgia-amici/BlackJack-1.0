7fac49ea87aa9c8cf23f9faf44453ddef396bbfc
Claudia <claudia.donovan89@gmail.com>
2014-12-12 12:04:30 +0000

    deck tested; refactored

--- spec/deckSpec.js
+++ spec/deckSpec.js
@@ -2,20 +2,23 @@ var Deck = require('../src/deck.js')
 
 describe("Deck", function(){
 
-	describe('before added to the game', function(){
+	var deck;
+
+	beforeEach(function(){
+		deck = new Deck;
+	});
+
+	describe('before being added to the game', function(){
 
 	 	it('it should be initialized with an empty array', function(){
-	 		var deck = new Deck();
 	 		expect(deck.readyDeck).toEqual([]);
 	 	});
 
 	 	it('it should be initialized with an array of suits', function(){
-	 		var deck = new Deck();
 	 		expect(deck.suits).toEqual(["flowers", "hearts", "diamonds", "spades"]);
 	 	});
 
 	 	it('it should be initialized with an array of values', function(){
-	 		var deck = new Deck();
 	 		expect(deck.faceValues).toEqual(["07", "08", "09", "10", "J10", "Q10", "K10", "A11"]);
 	 	});
 
@@ -24,7 +27,6 @@ describe("Deck", function(){
 	describe('before play begins', function(){
 
 	 	it('the suits and faceValues arrays are looped over to fill the empty readyDeck array with a complete game deck', function(){
-	 		var deck = new Deck();
 	 		deck.makeSuits()
 	 		expect(deck.readyDeck[0]).toEqual('flowers07')
 	 		expect(deck.readyDeck[31]).toEqual('spadesA11')
